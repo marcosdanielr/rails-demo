7c5aa37fb1d9f185dabea1cf3f024ac8b5b6b178
Marcos Roque <devmarcosdanielr@gmail.com>
2025-03-09 15:52:33 -0300

    feat: auth

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[show edit update destroy]
+  allow_unauthenticated_access only: %i[ index show ]
+  before_action :set_product, only: %i[ show edit update destroy ]
 
   def index
     @products = Product.all
@@ -44,6 +45,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.expect(product: [:name])
+    params.expect(product: [ :name ])
   end
 end
